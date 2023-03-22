0bde3ba7c51aa3969d1bc0c9159b3436852ad9e7
AdamJHowell <adam.j.howell@gmail.com>
2023-03-22 10:35:54 -0600

    Initial commit.

---
+++ WT32_ETH01_BasicEthernet.ino
@@ -0,0 +1,103 @@
+/**
+ * This sketch demonstrates basic Ethernet usage.
+ */
+
+#include <ETH.h>
+
+static bool eth_connected     = false;
+const char *serverAddress     = "theocho.local";
+const unsigned int serverPort = 1880;
+unsigned long lastTestTime    = 0;
+unsigned long testInterval    = 10000;
+
+
+/**
+ * @brief This is a callback function to handle network events.
+ * @param event a pre-defined event to process.
+ * Even though the event type is WiFiEvent_t, the same events work with the ETH.h library.
+ */
+void NetworkEvent( WiFiEvent_t event )
+{
+	switch( event )
+	{
+		case ARDUINO_EVENT_ETH_START:
+			Serial.println( "ETH Started" );
+			//set eth hostname here
+			ETH.setHostname( "esp32-ethernet" );
+			break;
+		case ARDUINO_EVENT_ETH_CONNECTED:
+			Serial.println( "ETH Connected" );
+			break;
+		case ARDUINO_EVENT_ETH_GOT_IP:
+			Serial.print( "ETH MAC: " );
+			Serial.print( ETH.macAddress() );
+			Serial.print( ", IPv4: " );
+			Serial.print( ETH.localIP() );
+			if( ETH.fullDuplex() )
+			{
+				Serial.print( ", FULL_DUPLEX" );
+			}
+			Serial.print( ", " );
+			Serial.print( ETH.linkSpeed() );
+			Serial.println( "Mbps" );
+			eth_connected = true;
+			break;
+		case ARDUINO_EVENT_ETH_DISCONNECTED:
+			Serial.println( "ETH Disconnected" );
+			eth_connected = false;
+			break;
+		case ARDUINO_EVENT_ETH_STOP:
+			Serial.println( "ETH Stopped" );
+			eth_connected = false;
+			break;
+		default:
+			break;
+	}
+}
+
+
+/**
+ * @brief The testClient() function will attempt to connect to a server and verify the connection succeeds.
+ * @param host the server address.
+ * @param port the server port.
+ */
+void testClient( const char *host, const unsigned int port )
+{
+	Serial.print( "\nConnecting to " );
+	Serial.println( host );
+
+	WiFiClient client;
+	if( !client.connect( host, port ) )
+	{
+		Serial.println( "connection failed" );
+		return;
+	}
+	client.printf( "GET / HTTP/1.1\r\nHost: %s\r\n\r\n", host );
+	while( client.connected() && !client.available() )
+		;
+	while( client.available() )
+	{
+		Serial.write( client.read() );
+	}
+
+	Serial.println( "closing connection\n" );
+	client.stop();
+}
+
+void setup()
+{
+	Serial.begin( 115200 );
+	WiFi.onEvent( NetworkEvent );
+	ETH.begin();
+}
+
+
+void loop()
+{
+	unsigned long currentTime = millis();
+	if( eth_connected && ( lastTestTime == 0 || ( currentTime > testInterval && ( currentTime - testInterval ) > lastTestTime ) ) )
+	{
+		testClient( serverAddress, serverPort );
+		lastTestTime = millis();
+	}
+}
