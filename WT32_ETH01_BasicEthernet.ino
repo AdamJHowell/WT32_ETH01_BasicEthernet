983cefe83daa058f9b94f7a13f00d34fc6f9f731
AdamJHowell <adam.j.howell@gmail.com>
2024-01-26 16:21:34 -0700

    Changed tabs to 3 spaces.

--- WT32_ETH01_BasicEthernet.ino
+++ WT32_ETH01_BasicEthernet.ino
@@ -24,38 +24,38 @@ uint8_t linkSpeed                = 42;                   // This default value h
  */
 void NetworkEvent( WiFiEvent_t event )
 {
-	networkCallbackCount++;
-	switch( event )
-	{
-		case ARDUINO_EVENT_ETH_START:
-			Serial.println( "ETH Started" );
-			ETH.setHostname( HOSTNAME );
-			break;
-		case ARDUINO_EVENT_ETH_CONNECTED:
-			Serial.println( "ETH Connected" );
-			break;
-		case ARDUINO_EVENT_ETH_GOT_IP:
-			snprintf( macAddress, 18, "%s", ETH.macAddress().c_str() );
-			snprintf( ipAddress, 16, "%d.%d.%d.%d", ETH.localIP()[0], ETH.localIP()[1], ETH.localIP()[2], ETH.localIP()[3] );
-			if( ETH.fullDuplex() )
-				snprintf( duplex, 12, "%s", "FULL_DUPLEX" );
-			else
-				snprintf( duplex, 12, "%s", "HALF_DUPLEX" );
-			linkSpeed = ETH.linkSpeed();
-			Serial.printf( "ETH MAC: %s, IPv4: %s, %s, %u Mbps", macAddress, ipAddress, duplex, linkSpeed );
-			eth_connected = true;
-			break;
-		case ARDUINO_EVENT_ETH_DISCONNECTED:
-			Serial.println( "ETH Disconnected" );
-			eth_connected = false;
-			break;
-		case ARDUINO_EVENT_ETH_STOP:
-			Serial.println( "ETH Stopped" );
-			eth_connected = false;
-			break;
-		default:
-			break;
-	}
+   networkCallbackCount++;
+   switch( event )
+   {
+      case ARDUINO_EVENT_ETH_START:
+         Serial.println( "ETH Started" );
+         ETH.setHostname( HOSTNAME );
+         break;
+      case ARDUINO_EVENT_ETH_CONNECTED:
+         Serial.println( "ETH Connected" );
+         break;
+      case ARDUINO_EVENT_ETH_GOT_IP:
+         snprintf( macAddress, 18, "%s", ETH.macAddress().c_str() );
+         snprintf( ipAddress, 16, "%d.%d.%d.%d", ETH.localIP()[0], ETH.localIP()[1], ETH.localIP()[2], ETH.localIP()[3] );
+         if( ETH.fullDuplex() )
+            snprintf( duplex, 12, "%s", "FULL_DUPLEX" );
+         else
+            snprintf( duplex, 12, "%s", "HALF_DUPLEX" );
+         linkSpeed = ETH.linkSpeed();
+         Serial.printf( "ETH MAC: %s, IPv4: %s, %s, %u Mbps", macAddress, ipAddress, duplex, linkSpeed );
+         eth_connected = true;
+         break;
+      case ARDUINO_EVENT_ETH_DISCONNECTED:
+         Serial.println( "ETH Disconnected" );
+         eth_connected = false;
+         break;
+      case ARDUINO_EVENT_ETH_STOP:
+         Serial.println( "ETH Stopped" );
+         eth_connected = false;
+         break;
+      default:
+         break;
+   }
 }  // End of the NetworkEvent() callback function.
 
 
@@ -66,49 +66,49 @@ void NetworkEvent( WiFiEvent_t event )
  */
 void testClient( const char *host, const unsigned int port )
 {
-	Serial.printf( "\nConnecting to %s\n", host );
-	Serial.printf( "ESP32 IP address: %s\n", ipAddress );
+   Serial.printf( "\nConnecting to %s\n", host );
+   Serial.printf( "ESP32 IP address: %s\n", ipAddress );
 
-	WiFiClient client;
-	if( !client.connect( host, port ) )
-	{
-		Serial.println( "connection failed" );
-		return;
-	}
-	client.printf( "GET / HTTP/1.1\r\nHost: %s\r\n\r\n", host );
-	while( client.connected() && !client.available() )
-		;
-	while( client.available() )
-	{
-		Serial.write( client.read() );
-	}
+   WiFiClient client;
+   if( !client.connect( host, port ) )
+   {
+      Serial.println( "connection failed" );
+      return;
+   }
+   client.printf( "GET / HTTP/1.1\r\nHost: %s\r\n\r\n", host );
+   while( client.connected() && !client.available() )
+      ;
+   while( client.available() )
+   {
+      Serial.write( client.read() );
+   }
 
-	Serial.println( "Closing connection\n" );
-	client.stop();
+   Serial.println( "Closing connection\n" );
+   client.stop();
 }  // End of the testClient() function.
 
 
 void setup()
 {
-	delay( 1000 );
-	Serial.begin( 115200 );
-	if( !Serial )
-		delay( 1000 );
-	Serial.println( "\n" );
-	Serial.println( "Function setup() is beginning." );
-	WiFi.onEvent( NetworkEvent );
-	ETH.begin();
-	// This delay give the Ethernet hardware time to initialize.
-	delay( 300 );
+   delay( 1000 );
+   Serial.begin( 115200 );
+   if( !Serial )
+      delay( 1000 );
+   Serial.println( "\n" );
+   Serial.println( "Function setup() is beginning." );
+   WiFi.onEvent( NetworkEvent );
+   ETH.begin();
+   // This delay give the Ethernet hardware time to initialize.
+   delay( 300 );
 }  // End of the setup() function.
 
 
 void loop()
 {
-	unsigned long currentTime = millis();
-	if( eth_connected && ( lastTestTime == 0 || ( currentTime > testInterval && ( currentTime - testInterval ) > lastTestTime ) ) )
-	{
-		testClient( serverAddress, serverPort );
-		lastTestTime = millis();
-	}
+   unsigned long currentTime = millis();
+   if( eth_connected && ( lastTestTime == 0 || ( currentTime > testInterval && ( currentTime - testInterval ) > lastTestTime ) ) )
+   {
+      testClient( serverAddress, serverPort );
+      lastTestTime = millis();
+   }
 }  // End of the loop() function.
